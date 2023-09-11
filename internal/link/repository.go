86c9c2c51c7ad3bf84d5aca4aa329a503ae37240
Ronilson Alves <falecom@ronilsonalves.com>
2023-09-11 16:58:27 -0300

    ✨ feat: Add initial version of link stats and add missing methods from CRUD
    – Implement the initial version of link stats (CountLinksByUser & CountLinkClicksByUser) at Link service.go and repository.go
    – Added missing methods to these files to READ,UPDATE and DELETE a shortened link.
    - Added fields createdAt, clicks to support initial version of link stats.

--- internal/link/repository.go
+++ internal/link/repository.go
@@ -1,14 +1,23 @@
 package link
 
 import (
+	"fmt"
+	"github.com/google/uuid"
 	"github.com/ronilsonalves/5lnk/internal/domain"
 	"gorm.io/gorm"
+	"strconv"
 )
 
 type Repository interface {
+	FindByID(id uuid.UUID) (*domain.Link, error)
 	FindByOriginal(original string) (*domain.Link, error)
 	FindByShortened(shortened string) (*domain.Link, error)
+	FindAllByUser(userId string) (*[]domain.Link, error)
+	CountLinksByUser(userId string) (int64, error)
+	CountLinkClicksByUser(userId string) (int64, error)
 	Create(link *domain.Link) error
+	Update(link *domain.Link) error
+	Delete(link *domain.Link) error
 }
 
 type linkRepository struct {
@@ -18,6 +27,15 @@ type linkRepository struct {
 // NewLinkRepository creates a new link repository
 func NewLinkRepository(db *gorm.DB) Repository {
 	return &linkRepository{db: db}
+}
+
+// FindByID finds a link by the ID
+func (r *linkRepository) FindByID(id uuid.UUID) (*domain.Link, error) {
+	var link domain.Link
+	if err := r.db.Where("id = ?", id).First(&link).Error; err != nil {
+		return nil, err
+	}
+	return &link, nil
 }
 
 // FindByOriginal finds a link by the original URL
@@ -35,10 +53,54 @@ func (r *linkRepository) FindByShortened(shortened string) (*domain.Link, error)
 	if err := r.db.Where("shortened = ?", shortened).First(&link).Error; err != nil {
 		return nil, err
 	}
+	go r.db.Model(&link).Update("clicks", link.Clicks+1)
 	return &link, nil
+}
+
+// FindAllByUser finds all links by user
+func (r *linkRepository) FindAllByUser(userId string) (*[]domain.Link, error) {
+	var links []domain.Link
+	if err := r.db.Where("user_id = ?", userId).Find(&links).Error; err != nil {
+		return nil, err
+	}
+	return &links, nil
+}
+
+// Update updates a link
+func (r *linkRepository) Update(link *domain.Link) error {
+	_, err := r.FindByID(link.ID)
+	if err != nil {
+		return fmt.Errorf("unable to find link %v", err)
+	}
+	return r.db.Model(&link).Update("original", link.Original).Error
 }
 
 // Create creates a new shortened URL
 func (r *linkRepository) Create(link *domain.Link) error {
 	return r.db.Create(link).Error
+}
+
+// CountLinksByUser counts the number of shortened links by user
+func (r *linkRepository) CountLinksByUser(userId string) (int64, error) {
+	var count int64
+	if err := r.db.Model(&domain.Link{}).Where("user_id = ?", userId).Count(&count).Error; err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+// CountLinkClicksByUser counts the number of clicks by user
+func (r *linkRepository) CountLinkClicksByUser(userId string) (int64, error) {
+	var total = ""
+	if err := r.db.Raw("SELECT SUM(clicks) as total FROM links WHERE user_id = ?", userId).Scan(&total).Error; err != nil {
+		return 0, fmt.Errorf("unable to count clicks by user")
+	}
+	var count int64
+	count, _ = strconv.ParseInt(total, 10, 64)
+	return count, nil
+}
+
+// Delete deletes a link
+func (r *linkRepository) Delete(link *domain.Link) error {
+	return r.db.Delete(link).Error
 }
--- internal/link/service.go
+++ internal/link/service.go
@@ -1,14 +1,23 @@
 package link
 
 import (
+	"fmt"
+	"github.com/google/uuid"
 	"github.com/ronilsonalves/5lnk/internal/domain"
 	"github.com/ronilsonalves/5lnk/pkg/web"
 	"math/rand"
+	"time"
 )
 
 type Service interface {
 	ShortenURL(request web.ShortenURL) (domain.Link, error)
+	GetLink(id string) (*domain.Link, error)
+	Update(request web.ShortenedURL) (domain.Link, error)
 	GetOriginalURL(shortened string) (string, error)
+	GetAllByUser(userId string) (*[]domain.Link, error)
+	Delete(request web.ShortenedURL) error
+	CountLinksByUser(userId string) (int64, error)
+	CountLinkClicksByUser(userId string) (int64, error)
 }
 
 type linkService struct {
@@ -18,6 +27,15 @@ type linkService struct {
 // NewLinkService creates a new link service
 func NewLinkService(repo Repository) Service {
 	return &linkService{repo: repo}
+}
+
+// GetLink returns a link by the ID
+func (s *linkService) GetLink(id string) (*domain.Link, error) {
+	linkId, err := uuid.Parse(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid link id")
+	}
+	return s.repo.FindByID(linkId)
 }
 
 // ShortenURL creates a new shortened URL
@@ -36,6 +54,8 @@ func (s *linkService) ShortenURL(request web.ShortenURL) (domain.Link, error) {
 		Original:  request.URL,
 		Shortened: shortened,
 		FinalURL:  "https://" + request.ShortDomain + "/" + shortened,
+		UserId:    request.UserId,
+		CreatedAt: time.Now(),
 	}
 
 	// Insert the link into the database
@@ -53,6 +73,47 @@ func (s *linkService) GetOriginalURL(shortened string) (string, error) {
 		return "", err
 	}
 	return link.Original, nil
+}
+
+// GetAllByUser returns all shortened links by user
+func (s *linkService) GetAllByUser(userId string) (*[]domain.Link, error) {
+	return s.repo.FindAllByUser(userId)
+}
+
+// Update updates a link
+func (s *linkService) Update(request web.ShortenedURL) (domain.Link, error) {
+	toUpdate, err := s.repo.FindByID(request.Id)
+	if err != nil {
+		return domain.Link{}, err
+	}
+
+	// For now, only the original URL can be updated
+	toUpdate.Original = request.Original
+
+	if err := s.repo.Update(toUpdate); err != nil {
+		return domain.Link{}, err
+	}
+
+	return *toUpdate, nil
+}
+
+// CountLinksByUser counts the number of shortened links by user
+func (s *linkService) CountLinksByUser(userId string) (int64, error) {
+	return s.repo.CountLinksByUser(userId)
+}
+
+// CountLinkClicksByUser counts the number of clicks by user
+func (s *linkService) CountLinkClicksByUser(userId string) (int64, error) {
+	return s.repo.CountLinkClicksByUser(userId)
+}
+
+// Delete deletes a link
+func (s *linkService) Delete(request web.ShortenedURL) error {
+	toDelete, err := s.repo.FindByID(request.Id)
+	if err != nil {
+		return fmt.Errorf("unable to delete link: %v", err)
+	}
+	return s.repo.Delete(toDelete)
 }
 
 // generateShortURL generates a random 6-character alphanumeric string for the shortened URL
