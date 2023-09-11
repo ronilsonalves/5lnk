e7a02aee8cc94353827230efc90c43f4dca4e341
Ronilson Alves <falecom@ronilsonalves.com>
2023-09-11 16:51:32 -0300

    ♻️ refactor: Update Link struct properties
    – Changed ID property from uint to UUID
    – Added userId as FK to map relation between links and user
    - Added fields createdAt, clicks to support initial version of link stats.

--- internal/domain/link.go
+++ internal/domain/link.go
@@ -1,8 +1,28 @@
 package domain
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
 type Link struct {
-	ID        uint   `gorm:"primaryKey"`
-	Original  string `gorm:"uniqueIndex"`
-	Shortened string `gorm:"uniqueIndex"`
-	FinalURL  string
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	Original  string    `gorm:"uniqueIndex" json:"original"`
+	Shortened string    `gorm:"uniqueIndex" json:"shortened"`
+	FinalURL  string    `json:"finalUrl"`
+	UserId    string    `gorm:"index" json:"userId"`
+	CreatedAt time.Time `json:"createdAt"`
+	Clicks    int       `json:"clicks"`
+}
+
+// BeforeCreate initialize UUID and set 0 as initial value for links' click.
+func (Link *Link) BeforeCreate(scope *gorm.DB) error {
+	id, err := uuid.NewRandom()
+	if err != nil {
+		return err
+	}
+	scope.Statement.SetColumn("id", id)
+	scope.Statement.SetColumn("clicks", 0)
+	return nil
 }
