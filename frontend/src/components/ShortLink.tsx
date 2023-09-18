fe88fcd809d2164bf43ebfe20ecec2c5ddcd09bd
Ronilson Alves <falecom@ronilsonalves.com>
2023-09-18 13:18:48 -0300

    🐛 fix: Error of not closing the URL Shortener modal at home
    – Updated the URL shortener component for unauthenticated users to fix the not closing modal component.

--- frontend/src/components/ShortLink.tsx
+++ frontend/src/components/ShortLink.tsx
@@ -10,27 +10,35 @@ declare global {
 export default function ShortLink() {
   const [shortUrl, setShortUrl] = useState("");
   const [copySuccess, setCopySuccess] = useState(false);
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const response = await fetch(process.env.NEXT_PUBLIC_API_URL+"links", {
+    const response = await fetch("/api/public_links", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
         url: (e.target as HTMLFormElement).url.value,
-        domain: "5lnk.me",
+        domain: process.env.NEXT_PUBLIC_DEFAULT_SHORT_DOMAIN,
       }),
     });
 
-    if (response.status === 201) {
-      const data = await response.json();
-      setShortUrl(data.FinalURL);
-      (e.target as HTMLFormElement).reset();
-      window.short_url_form.close();
+    const data = await response.json();
+
+    if (response.status !== 201) {
+      setError(data.message);
+      setTimeout(() => {
+        setError("");
+      }, 1500);
+      return;
     }
+    
+    setShortUrl(data.finalUrl);
+    (e.target as HTMLFormElement).reset();
+    window.short_url_form.close();
   };
 
   const handleCopy = async (e: React.MouseEvent<Element, MouseEvent>) => {
@@ -50,58 +58,91 @@ export default function ShortLink() {
             Shorten URL
           </button>
           <dialog id="short_url_form" className="modal">
-            <form method="dialog" className="modal-box" onSubmit={handleSubmit}>
-              <h3 className="font-bold text-lg">Shorten your URL!</h3>
-              <p className="py-4">
-                Please, paste the URL you want to shorten in the field below.
-              </p>
-              <div className="join">
-                <input
-                  name="url"
-                  type="url"
-                  required={true}
-                  className="input input-bordered join-item rounded-l-full w-full"
-                  placeholder="https://example.com/"
-                />
-                <button type="submit" className="btn join-item rounded-r-full w-1/4">
-                  Shorten URL
-                </button>
+              <form
+                method="dialog"
+                className="modal-box"
+                onSubmit={handleSubmit}
+              >
+                <h3 className="font-bold text-lg">Shorten your URL!</h3>
+                <p className="py-4">
+                  Please, paste the URL you want to shorten in the field below.
+                </p>
+                <div className="join w-full">
+                  <input
+                    name="url"
+                    type="url"
+                    required={true}
+                    className="input input-bordered join-item rounded-l-full w-full"
+                    placeholder="https://example.com/"
+                  />
+                  <button
+                    type="submit"
+                    className="btn join-item rounded-r-full w-1/4"
+                  >
+                    Shorten URL
+                  </button>
+                </div>
+              </form>
+              <form method="dialog" className="modal-backdrop">
+                <button>Close</button>
+              </form>
+            </dialog>
+          {error && (
+            <div className="toast toast-end">
+              <div className="alert alert-error">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>{error}</span>
               </div>
-            </form>
-          </dialog>
+            </div>
+          )}
         </div>
       )}
       {shortUrl && (
         <div className="container w-full">
-        <div className="join join-vertical w-full">
-          <div className="mockup-browser border bg-base-300 max-w-sm">
-            <div className="mockup-browser-toolbar">
-              <div className="input">{shortUrl.replace("https://", "")}</div>
+          <div className="join join-vertical w-full">
+            <div className="mockup-browser border bg-base-300 max-w-sm">
+              <div className="mockup-browser-toolbar">
+                <div className="input">{shortUrl.replace("https://", "")}</div>
+              </div>
             </div>
-          </div>
-          {copySuccess ? (
-            <div className="alert alert-success">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="stroke-current shrink-0 h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
+            {copySuccess ? (
+              <div className="alert alert-success">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>Your shorten URL has been copied!</span>
+              </div>
+            ) : (
+              <button
+                className="btn btn-primary"
+                onClick={(e) => handleCopy(e)}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span>Your shorten URL has been copied!</span>
-            </div>
-          ) : (
-            <button className="btn btn-primary" onClick={(e) => handleCopy(e)}>
-              Copy link
-            </button>
-          )}
-        </div>
+                Copy link
+              </button>
+            )}
+          </div>
         </div>
       )}
     </>
