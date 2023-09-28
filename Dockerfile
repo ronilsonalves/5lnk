0cc859e6d08c0b8f61a9271318cef79a45b4948e
Ronilson Alves <falecom@ronilsonalves.com>
2023-09-28 16:44:25 -0300

    🚀 feat: Update Firebase Init
    – Update Firebase Init to use ENV var
    – AND Update Dockerfile to reflects this change

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,14 @@ COPY go.mod ./
 COPY go.sum ./
 RUN go mod download
 COPY . ./
-RUN go mod download
 RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o 5lnk cmd/server/api.go
 
 # Final stage
 FROM gcr.io/distroless/static-debian10
 COPY --from=build /app/5lnk /5lnk
+COPY env /env
 COPY .env /
+ENV GOOGLE_APPLICATION_CREDENTIALS=env/firebase.json
 USER nonroot:nonroot
 EXPOSE 8080
 ENTRYPOINT ["/5lnk"]
--- config/auth/firebase.go
+++ config/auth/firebase.go
@@ -3,14 +3,12 @@ package auth
 import (
 	"context"
 	firebase "firebase.google.com/go/v4"
-	"google.golang.org/api/option"
 	"log"
 )
 
 // InitializeFirebase initializes the firebase app with Admin SDK.
 func InitializeFirebase(c context.Context) (*firebase.App, error) {
-	opt := option.WithCredentialsFile("env/firebase.json")
-	app, err := firebase.NewApp(c, nil, opt)
+	app, err := firebase.NewApp(c, nil)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n\n", err)
 	}
